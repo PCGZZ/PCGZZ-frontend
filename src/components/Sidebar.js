c9a16ce99b5d287eb9103d7f04ca845de9441f24
krispi1229 <112258809+krispi1229@users.noreply.github.com>
2024-08-27 11:34:47 +1000

    fix siderbar, waiting for connect to backend

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import '../styles/sidebar.css';
 import AssignmentIcon from '@mui/icons-material/Assignment';
 import Face2Icon from '@mui/icons-material/Face2';
@@ -7,6 +7,13 @@ import LogoutIcon from '@mui/icons-material/Logout';
 
 function Sidebar() {
   const location = useLocation();
+  const navigate = useNavigate(); // Initialize useNavigate
+
+  const handleLogout = () => {
+    // Redirect to the login page
+    navigate('/');
+  };
+
   return (
     <div className="sidebar">
       <div className="logo-design">
@@ -33,7 +40,17 @@ function Sidebar() {
           <div className="sidebar-link-text-white">Virtual Adult</div>
         </Link>
       </div>
-      <div className="logout-frame">
+      <div
+        className="logout-frame"
+        role="button"// Adding role for accessibility
+        tabIndex={0}// Making it focusable with keyboard
+        onClick={handleLogout}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter' || e.key === ' ') {
+            handleLogout(); // Trigger logout on Enter or Space key
+          }
+        }}
+      >
         <div className="logout-button">
           <div className="logout-icon">
             <LogoutIcon sx={{ mr: 2, color: 'var(--text)' }} />
