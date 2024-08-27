b96b7d6467fb38c527eb20f7658873120fdbdb9d
krispi1229 <112258809+krispi1229@users.noreply.github.com>
2024-08-27 11:08:55 +1000

    fix bug siderbar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -15,23 +15,37 @@ function Sidebar() {
     <div className="sidebar">
       <div className="logo-design">Virtual Adults.</div>
       <div className="sidebar-links">
-        <Link to="/assignments" className={`sidebar-link ${location.pathname === '/assignments' ? 'active' : 'inactive'}`}>
+        <Link
+          to="/assignments"
+          className={`sidebar-link ${location.pathname === '/assignments' ? 'active' : 'inactive'}`}
+        >
           <div className="sidebar-link-icon">
             {/* Add icon here */}
           </div>
           <div className="sidebar-link-text-white">Assignment</div>
         </Link>
-        <Link to="/virtual-adult" className={`sidebar-link ${location.pathname === '/virtual-adult' ? 'active' : 'inactive'}`}>
+        <Link
+          to="/virtual-adult"
+          className={`sidebar-link ${location.pathname === '/virtual-adult' ? 'active' : 'inactive'}`}
+        >
           <div className="sidebar-link-icon">
             {/* Add icon here */}
           </div>
           <div className="sidebar-link-text-white">Virtual Adult</div>
         </Link>
       </div>
-      <div className="logout-frame">
-        <div className="logout-button" onClick={handleLogout}>
-          {' '}
-          {/* Add onClick */}
+      <div
+        className="logout-frame"
+        onClick={handleLogout}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter' || e.key === ' ') {
+            handleLogout();
+          }
+        }}
+        role="button"
+        tabIndex={0}
+      >
+        <div className="logout-button">
           <div className="logout-icon">
             {/* Add log out icon here */}
           </div>
