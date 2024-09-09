37cf9fdaf0f9e1e26b6a6cbcbd081e60bc6f0c3d
krispi1229 <112258809+krispi1229@users.noreply.github.com>
2024-09-09 23:04:55 +1000

    feat(assignment): add teacher login bottom and first login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
+import { useNavigate } from 'react-router-dom'; // Import navigate for internal routing
 import '../styles/Login.css';
 import deakinLogo from '../styles/image/deakin-university.png';
 
 function Login() {
   const { loginWithRedirect, isAuthenticated, isLoading } = useAuth0();
-  const handleLogin = () => {
+  const navigate = useNavigate(); // Hook for navigation
+
+  const handleStudentLogin = () => {
     loginWithRedirect({
       redirectUri: `${window.location.origin}/assignments`, // redirect to assignments page after login
     });
+  };
+
+  const handleTeacherLogin = () => {
+    navigate('/teacher-login'); // Navigate to the teacher login page
   };
 
   if (isLoading) {
@@ -25,8 +32,20 @@ function Login() {
             className="deakin-logo"
           />
           <h1>Welcome</h1>
-          <button className="login-button" onClick={handleLogin} type="button">
+          <button
+            className="login-button"
+            onClick={handleStudentLogin}
+            type="button"
+          >
             Log in
+          </button>
+          {/* Add Teacher Login button below the student login */}
+          <button
+            className="teacher-login-button"
+            onClick={handleTeacherLogin}
+            type="button"
+          >
+            Teacher Log in
           </button>
         </div>
         <div className="login-right">
